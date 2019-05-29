c9d2678f66111388836df73fac7248e05daf13ce
Max Trestman <mtrestman1@gmail.com>
2019-05-29 16:03:30 -0400

    Initial commit

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+
+
+
+'''
+m = Player("Max", 99)
+print(m.name)
+print(m.level)
+'''
